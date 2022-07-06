85204f971fcf3b3a9a2573eee98215bab10d33f0
Максим Иванов <tarakah@gmail.com>
2022-07-07 00:08:12 +0300

    Создал первый тест checkTitleSwagLabs

---
+++ Web-Tests/Swag Labs/checkTitleSwagLabs.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+
+namespace Web_Tests.Swag_Labs
+{
+    [TestFixture]
+    public class checkTitleSwagLabs
+    {
+        private IWebDriver _webDriver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            _webDriver = new ChromeDriver();
+        }
+
+        [Test]
+        public void checkTitleSwagLabsChromeTest()
+        {
+            _webDriver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            Assert.That(_webDriver.Title.Contains("Swag Labs"));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _webDriver.Quit();
+        }
+
+    }
+}
